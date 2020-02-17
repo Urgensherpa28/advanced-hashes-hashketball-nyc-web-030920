091efce4d895abf8759b24fa1a6fab5ce09c7be8
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-17 20:35:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -212,7 +212,7 @@ end
 def most_points_scored
   points_scored = 0
   player = "" 
-    game_hash.each do | team, attributes|
+    hash.each do | team, attributes|
       attributes[:players].each do |stats|
         if stats[:points] > points_scored
           points_scored = stats[:points]
